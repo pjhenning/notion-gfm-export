bfc15db0f805782231368e1c622720358c5cb27e
PJ Henning <pjhenning@fastmail.com>
2025-01-17 00:00:58 -0800

    Trying to figure out this auth thing

--- src/routes/notionauth/+page.server.ts
+++ src/routes/notionauth/+page.server.ts
@@ -1,11 +1,38 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ params, url }) => {
+interface NotionAuthResponseJSON {
+	access_token: string;
+}
+
+export const load: PageServerLoad = async ({ url }) => {
+  const clientId = process.env.oauth_client_id;
+  const clientSecret = process.env.oauth_client_secret;
+  const redirectUri = 'https://intrapology-notion-gfm-export.onrender.com/notionauth';
+
+	const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+
+	const response = await fetch("https://api.notion.com/v1/oauth/token", {
+		method: "POST",
+		headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+		Authorization: `Basic ${encoded}`,
+	},
+		body: JSON.stringify({
+			grant_type: "authorization_code",
+			code: "your-temporary-code",
+			redirect_uri: redirectUri,
+		}),
+	});
+
+	const responseJSON: NotionAuthResponseJSON = await response.json();
+
 	return {
 		title: 'Hello world!',
 		content: 'Welcome to our blog. Lorem ipsum dolor sit amet...',
-		searchParams: [...url.searchParams.entries()]
+		searchParams: [...url.searchParams.entries()],
+		auth: responseJSON.access_token
 	};
 
 	error(404, 'Not found');
--- src/routes/notionauth/+page.svelte
+++ src/routes/notionauth/+page.svelte
@@ -3,6 +3,7 @@
 
 	let { data }: { data: PageData } = $props();
 	console.log(data.searchParams);
+	console.log(data.auth);
 </script>
 
 <h1>Notion Authorization</h1>
