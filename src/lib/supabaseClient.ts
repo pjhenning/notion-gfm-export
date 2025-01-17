a75d4a3a7d3d0f5042591d341bb52e45673940bc
PJ Henning <pjhenning@fastmail.com>
2025-01-17 01:06:50 -0800

    db interface

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from './supabaseTypes';
 
 const project = process.env.supabase_project_url;
 const key = process.env.supabase_anon_key;
 
-export const supabase = createClient(`https://${project}.supabase.co`, key!);
+export const supabase = createClient<Database>(`https://${project}.supabase.co`, key!);
