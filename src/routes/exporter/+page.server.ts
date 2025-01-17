963dc3b17f8f6ceed92d35497eddbb603630764b
PJ Henning <pjhenning@fastmail.com>
2025-01-17 00:58:22 -0800

    Have to store token somewhere :/

--- src/routes/exporter/+page.server.ts
+++ src/routes/exporter/+page.server.ts
@@ -1,10 +1,16 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { supabase } from '$lib/supabaseClient';
 
-export const load: PageServerLoad = ({ params }) => {
+export const load: PageServerLoad = async () => {
+  // TODO: get token
+
+  const sb = await supabase.from("main").select();
+  const sbData = sb.data;
+
 	return {
-    title: 'Hello world!',
-    content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+    sbData,
+    token: ''
   };
 
 	error(404, 'Not found');
--- src/routes/exporter/+page.svelte
+++ src/routes/exporter/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+  import { getMarkdownForPage } from '$lib/gfm-export';
 
 	let { data }: { data: PageData } = $props();
+
+	let pageID = $state('Page ID');
+
+	console.log('sbData:');
+	console.log(data.sbData);
+
+	async function doExport() {
+		console.log('its happening...');
+		getMarkdownForPage(pageID, data.token);
+	}
 </script>
 
 <h1>Exporter</h1>
+
+<input type="text" bind:value={pageID}>
+<button type="submit" onclick={doExport}>Export</button>
