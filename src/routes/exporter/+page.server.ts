31eb81202a805404aadab4e2ec3fa0f0a60c5eaa
PJ Henning <pjhenning@fastmail.com>
2025-01-17 18:03:03 -0800

    Use env var for page id for now

--- src/routes/exporter/+page.server.ts
+++ src/routes/exporter/+page.server.ts
@@ -2,12 +2,10 @@ import type { Actions } from './$types';
 import { supabase } from '$lib/supabaseClient';
 import { getMarkdownForPage } from '$lib/gfm-export';
 
-const pageID = '17bddd82c25580f38e27e2376678fe30';
-
 export const actions = {
 	default: async (_event) => {
     const sb = await supabase.from("main").select();
-		const md = await getMarkdownForPage(pageID, sb.data![0].auth);
+		const md = await getMarkdownForPage(process.env.pageid!, sb.data![0].auth);
     return { md };
 	}
 } satisfies Actions;
